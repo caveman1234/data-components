21baddb3ec8dc691da4f3eca54b85e5abefa62d4
zt_diaoxr <diaoxinrui@qq.com>
2019-03-16 23:05:35 +0800

    develop1-第一次提交

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import MainPage from '@/components/MainPage'
 import CompContainer from '@/components/CompContainer'
 var a = "develop1";
-var a = "develop1111";
+var a = "develop1111----------------1";
 Vue.use(Router)
 
 export default new Router({
