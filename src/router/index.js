03b71d437bb3aa70355f426e763343ed2eba595c
zt_diaoxr <diaoxinrui@qq.com>
2019-03-16 22:39:35 +0800

    develop1-commit-2

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import MainPage from '@/components/MainPage'
 import CompContainer from '@/components/CompContainer'
 var a = "develop1";
+var a = "develop1";
 Vue.use(Router)
 
 export default new Router({
