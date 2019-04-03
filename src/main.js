454814f9b25999e235f6fe5995dcfc2786fb521e
zt_diaoxr <diaoxinrui@qq.com>
2019-04-03 20:20:41 +0800

    combine to one commit

--- src/main.js
+++ src/main.js
@@ -10,14 +10,14 @@ import Hello from "./components/Hello.vue";
 import Dialog1 from "./components/Dialog1.vue";
 import Dialog2 from "./components/Dialog2.vue";
 
-import  { VueCreateAPI } from './vue-create-api.js';
+// import  { VueCreateAPI } from './vue-create-api.js';
 
-Vue.use(VueCreateAPI)
+// Vue.use(VueCreateAPI)
 Vue.use(Element)
 
-Vue.createAPI(Hello);
-Vue.createAPI(Dialog1);
-Vue.createAPI(Dialog2);
+// Vue.createAPI(Hello);
+// Vue.createAPI(Dialog1);
+// Vue.createAPI(Dialog2);
 
 Vue.config.productionTip = false
 
